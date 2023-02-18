0d5b499b017f626999b5c9e3f8ac660614e09095
MR-D E R K <107317634+MR-DARK54@users.noreply.github.com>
2023-02-18 11:51:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ the url it will send you 2 links which you can use to track people.
 \n1. Cloudflare Link: This method will show a cloudflare under attack page to gather informations and afterwards victim will be redirected to destinationed URL.
 \n2. Webview Link: This will show a website (ex bing , dating sites etc) using iframe for gathering information.
 ( ⚠️ Many sites may not work under this method if they have x-frame header present.Ex https://google.com )
-\n\nThe project is OSS at: https://github.com/Th30neAnd0nly/TrackDown
+\n\nThe project is OSS at: https://github.com/MR-DARK54
 `);
 }
   
